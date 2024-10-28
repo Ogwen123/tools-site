06fc5fc6425cc775635c36908e5441df69c55b8e
Ogwen123 <owen.d.jones@btinternet.com>
2024-10-28 16:21:54 +0000

    generate maps

--- src/components/routes/visual/MapsVisualiser.tsx
+++ src/components/routes/visual/MapsVisualiser.tsx
@@ -3,12 +3,40 @@ import React from 'react'
 const MapsVisualiser = () => {
 
     const [sets, setSets] = React.useState<{ a: string[], b: string[] }>({ a: [], b: [] })
-    const [maps, setMaps] = React.useState<{ a: string[], b: string[] }>({ a: [], b: [] })
+    const [maps, setMaps] = React.useState<string[][]>([])
     const [settings, setSettings] = React.useState<{ injective: boolean, surjective: boolean }>({ injective: false, surjective: false })
 
-    React.useEffect(() => {
+    const cartesian = (...a: any[]) => a.reduce((a, b) => a.flatMap((d: any) => b.map((e: any) => [d, e].flat())));
 
-    }, [sets])
+
+    const generateMaps = () => {
+        //let pairs = []
+        //for (let i of sets.a) {
+        //    let buffer = []
+        //    for (let j of sets.b) {
+        //        buffer.push([i, j])
+        //    }
+        //    pairs.push(buffer)
+        //}
+        //console.log(pairs)
+        let args = []
+
+        for (let _ in sets.a) {
+            args.push(sets.b)
+        }
+
+        const cartB = cartesian(...args)
+
+        let map: string[][] = []
+
+        for (let i of cartB) {
+            map.push(i.map(function (val: any, index: any) {
+                return [sets.a[index], val];
+            }))
+        }
+
+        setMaps(map)
+    }
 
     return (
         <div className='flex items-center flex-col h-full'>
@@ -20,8 +48,31 @@ const MapsVisualiser = () => {
             </div>
             <div className='w-full flex flex-row h-full mt-[10px]'>
                 <div className='flex flex-col flex-grow h-full mr-[5px]'>
-                    <div className='w-full h-[30%] border-w rounded-md mb-[5px] p-[10px]'>
-                        <div className='text-textlight text-sm mb-[10px]'>Seperate values in the set using commas.</div>
+                    <div className='w-full flex flex-col h-[35%] border-w rounded-md mb-[5px] p-[10px] overflow-auto'>
+
+                        <div className='w-full flex flex-row justify-between mb-[10px]'>
+                            <div className='text-textlight text-sm'>Seperate values in the set using commas.</div>
+                            <div className='justify-evenly flex flex-row w-1/2'>
+                                <div className='fc'>
+                                    Injective
+                                    <input
+                                        type="checkbox"
+                                        className='ml-[10px] accent-main'
+                                        checked={settings.injective}
+                                        onChange={(e) => setSettings((settings) => ({ ...settings, injective: e.target.checked }))}
+                                    />
+                                </div>
+                                <div className='fc'>
+                                    Surjective
+                                    <input
+                                        type="checkbox"
+                                        className='ml-[10px] accent-main'
+                                        checked={settings.surjective}
+                                        onChange={(e) => setSettings((settings) => ({ ...settings, surjective: e.target.checked }))}
+                                    />
+                                </div>
+                            </div>
+                        </div>
                         <input
                             type="text"
                             placeholder="Set 1"
@@ -38,29 +89,16 @@ const MapsVisualiser = () => {
                                 setSets((set) => ({ ...set, b: e.target.value.split(",") }))
                             }}
                         />
-                        <div className='w-full flex flex-row justify-evenly mt-[10px]'>
-                            <div className='fc'>
-                                Injective
-                                <input
-                                    type="checkbox"
-                                    className='ml-[10px] accent-main'
-                                    checked={settings.injective}
-                                    onChange={(e) => setSettings((settings) => ({ ...settings, injective: e.target.checked }))}
-                                />
-                            </div>
-                            <div className='fc'>
-                                Surjective
-                                <input
-                                    type="checkbox"
-                                    className='ml-[10px] accent-main'
-                                    checked={settings.surjective}
-                                    onChange={(e) => setSettings((settings) => ({ ...settings, surjective: e.target.checked }))}
-                                />
-                            </div>
-                        </div>
+                        <button className='button mt-auto' onClick={generateMaps}>Generate Maps</button>
                     </div>
-                    <div className='w-full h-[70%] border-w rounded-md mt-[5px] p-[10px]'>
-                        map outputs
+                    <div className='w-full h-[65%] border-w rounded-md mt-[5px] p-[10px]'>
+                        {maps.map((map, index) => {
+                            return (
+                                <div key={index}>
+                                    {map}
+                                </div>
+                            )
+                        })}
                     </div>
                 </div>
                 <div className='h-full flex-grow border-w rounded-md ml-[5px] p-[10px]'>
